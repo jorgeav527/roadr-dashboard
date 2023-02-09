85e1308cf5bc12d62e91cae2485f7e0c62e818b1
jorgeav527 <jorgeav527@gmail.com>
2023-02-08 20:45:26 -0500

    changing ...from dash import dcc... & ...from dash import html...

--- app/dashapp1/layout.py
+++ app/dashapp1/layout.py
@@ -1,19 +1,23 @@
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 
 
-layout = html.Div(id='main', children=[
-    html.H1(id='username'),
-    html.H1('Stock Tickers'),
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Coke', 'value': 'COKE'},
-            {'label': 'Tesla', 'value': 'TSLA'},
-            {'label': 'Apple', 'value': 'AAPL'}
-        ],
-        value='COKE'
-    ),
-    dcc.Graph(id='my-graph'),
-    dcc.Store(id='user-store'),
-], style={'width': '500'})
+layout = html.Div(
+    id="main",
+    children=[
+        html.H1(id="username"),
+        html.H1("Stock Tickers"),
+        dcc.Dropdown(
+            id="my-dropdown",
+            options=[
+                {"label": "Coke", "value": "COKE"},
+                {"label": "Tesla", "value": "TSLA"},
+                {"label": "Apple", "value": "AAPL"},
+            ],
+            value="COKE",
+        ),
+        dcc.Graph(id="my-graph"),
+        dcc.Store(id="user-store"),
+    ],
+    style={"width": "500"},
+)
